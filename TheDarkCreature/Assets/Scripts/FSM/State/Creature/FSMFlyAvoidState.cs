9df7c357bccf043376bc648a14b4f59f511ce3c2
Co Chi Tam <cochitam1990@gmail.com>
2016-01-30 23:58:14 +0700

    Fix Fly avoid state

--- TheDarkCreature/Assets/Scripts/FSM/State/Creature/FSMFlyAvoidState.cs
+++ TheDarkCreature/Assets/Scripts/FSM/State/Creature/FSMFlyAvoidState.cs
@@ -11,10 +11,21 @@ public class FSMFlyAvoidState : FSMBaseState
 	public override void StartState()
 	{
 		m_Controller.SetAnimation(EAnimation.Run);
-		var direction = (m_Controller.TransformPosition - m_Controller.GetEnemyPosition()).normalized;
-		var newTarget = m_Controller.TransformPosition + direction * (m_Controller.GetDetectRange() + 10f);
-		newTarget.y = 0f;
-		m_Controller.SetTargetPosition(newTarget);
+//		var direction = (m_Controller.TransformPosition - m_Controller.GetEnemyPosition()).normalized;
+//		var newTarget = m_Controller.TransformPosition + direction * (m_Controller.GetDetectRange() + 10f);
+//		newTarget.y = 0f;
+//		m_Controller.SetTargetPosition(newTarget);
+
+		var groupCtrl = m_Controller.GetGroupEntity();
+		var groupPosition = m_Controller.GetStartPosition();
+		var groupRadius = 50f;
+		if (groupCtrl != null)
+		{
+			groupRadius = groupCtrl.GetGroupRadius();
+			groupPosition = m_Controller.GetGroupEntity().GetTransformPosition();
+		}
+		var random = Random.insideUnitCircle * groupRadius; // TDCUltilities.RandomAround(groupRadius);
+		m_Controller.SetTargetPosition (groupPosition + new Vector3(random.x, 0f, random.y));
 		m_Controller.CallBackEvent("OnAvoid");
 	}
 
